480100296ca49c618b7276dfa5b405a951c3c2d6
Henry Pinkard <7969470+henrypinkard@users.noreply.github.com>
2023-10-31 18:53:25 -0700

    experiment backup

--- encoding_information/information_estimation.py
+++ encoding_information/information_estimation.py
@@ -191,8 +191,8 @@ def run_bootstrap(data, estimation_fn, num_bootstrap_samples=200, confidence_int
             data_samples = {}
             for k, v in data.items():
                 key, subkey = jax.random.split(key)
-                random_indices = jax.random.choice(subkey, np.arange(data.shape[0]), shape=(N,), replace=True)
-                data_sample = data[random_indices, ...]
+                random_indices = jax.random.choice(subkey, np.arange(v.shape[0]), shape=(N,), replace=True)
+                data_samples[k] = v[random_indices, ...]
             results.append(estimation_fn(**data_samples))
         
     results = np.array(results)
