d6ddf34b494689db08e787b4f9c753803871a2d2
emarkley <emarkley@berkeley.edu>
2024-09-17 13:26:04 -0700

    Adding hyperspectral metalens imaging dataset class

--- src/encoding_information/datasets/__init__.py
+++ src/encoding_information/datasets/__init__.py
@@ -1,4 +1,4 @@
 from .bsccm_utils import BSCCMDataset
 from .mnist_dataset import MNISTDataset
 from .cfa_dataset import ColorFilterArrayDataset
-from .HML_dataset import HML_Dataset
+from .HMLdataset import HMLDataset
